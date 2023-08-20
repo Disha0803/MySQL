958c636a685a2dd74d253ea256f0ce4976973095
unknown <mdishabls@gmail.com>
2023-08-20 13:28:19 +0530

    Day-1

---
+++ 1.sql
@@ -0,0 +1,12 @@
+create database university;
+use university;
+
+create table student_info(
+	roll int,
+    name varchar(30),
+    course varchar(20),
+    mark int,
+    gender char
+);
+
+select * from student_info;	
